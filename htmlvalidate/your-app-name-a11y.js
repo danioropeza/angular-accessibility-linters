e6d0990c647fc167ddf611b9c4cdd539496b4c05
Jose Daniel Oropeza Soria <danioropezasoria@gmail.com>
2022-12-07 20:17:45 -0400

    Add base project and htmlvalidate folder

---
+++ htmlvalidate/your-app-name-a11y.js
@@ -0,0 +1,35 @@
+
+var htmlValidate = require('html-validate');
+
+module.exports = htmlValidate.definePlugin({
+  name: "cxws-a11y",
+  configs: {
+    recommended: {
+      rules: {
+        "area-alt": "error",
+        "aria-hidden-body": "error",
+        "aria-label-misuse": "error",
+        "deprecated-rule": "error",
+        "empty-heading": "error",
+        "empty-title": "error",
+        "input-missing-label": "error",
+        "meta-refresh": "error",
+        "multiple-labeled-controls": "error",
+        "no-autoplay": "error",
+        "no-dup-id": "error",
+        "no-redundant-for": "error",
+        "no-redundant-role": "error",
+        "prefer-native-element": "error",
+        "svg-focusable": "error",
+        "text-content": "error",
+        "wcag/h30": "error",
+        "wcag/h32": "error",
+        "wcag/h36": "error",
+        "wcag/h37": "error",
+        "wcag/h63": "error",
+        "wcag/h67": "error",
+        "wcag/h71": "error",
+      },
+    },
+  },
+});
